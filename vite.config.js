62e3850bc3ece3d86082e2b9fa944c4cf5b57055
emmanuel <eminentia.ea@gmail.com>
2025-09-26 16:20:18 +0100

    github pages commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/nigerian_mini_bakery",
+  base: process.env.VITE_BASE_PATH || "/mini_bakery",
 });
